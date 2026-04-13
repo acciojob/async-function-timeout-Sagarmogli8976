e2d855fd67fc60bee04899243f78a48768a9c6f0
Sagarmogli8976 <114072291+Sagarmogli8976@users.noreply.github.com>
2026-04-14 02:12:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+function wait(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function handleSubmit() {
+  const text = textInput.value;
+  const delay = Number(delayInput.value);
+
+  output.innerText = "";
+
+  await wait(delay);
+
+  output.innerText = text;
+}
+
+button.addEventListener("click", handleSubmit);
